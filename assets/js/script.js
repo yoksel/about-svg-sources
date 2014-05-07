084ed759a68fa501b9d0d401fa0ed2da943a4da1
yoksel <yoksel@yandex.ru>
2014-05-07 23:49:56 +0400

    Save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,26 +103,20 @@ var popupImgClass = popupClass + "__img";
 // Toggle Codes
 // -------------------------------
 
-jQuery.fn.animateAuto = function(prop, speed, callback){
-    var elem, height, width;
-    return this.each(function(i, el){
-        el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
-        height = elem.css("height"),
-        width = elem.css("width"),
-        elem.remove();
+$(".js-toggle-code").each(function(){
+    var elemHeight = $(this).height();
+    $(this).attr("data-height", elemHeight);
+    $(this).height(elemHeight);
+});
 
-        if(prop === "height")
-            el.animate({"height":height}, speed, callback);
-        else if(prop === "width")
-            el.animate({"width":width}, speed, callback);
-        else if(prop === "both")
-            el.animate({"width":width,"height":height}, speed, callback);
-    });
-}
-
-$(".js-toggle-code").bind("click", function(e){
-    $(this).animateAuto("height", 1000);
-    console.log("123")
+$(".js-toggle-code").click(function(){
+    $(this).toggleClass("is--collapsed");
+    if ($(this).height() == $(this).attr("data-height")){
+        $(this).height("1.8em");
+    }
+    else {
+        $(this).height($(this).attr("data-height"));
+    }
 });
 
 
