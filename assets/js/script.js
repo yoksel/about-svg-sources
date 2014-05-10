b209b76a53605e220475eb919b5564f036f8868c
yoksel <yoksel@yandex.ru>
2014-05-11 00:43:01 +0400

    Save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,26 @@ function removeBackground(elem){
     $(elem).parent().addClass("no-highlight");
 }
 
+function setAttr(propName, propValue, editedElem) {
+
+    if (propName == "viewbox"
+        || propName == "preserveaspectratio"
+        || propName == "preserveAspectRatio"){
+       var svgElem = document.querySelector(".active svg");
+    }
+
+    if (propName == "viewbox"){
+        svgElem.setAttribute("viewBox", propValue);
+        }
+    else if (propName == "preserveaspectratio"
+        || propName == "preserveAspectRatio"){
+         svgElem.setAttribute("preserveAspectRatio", propValue);
+        }
+    else {
+        $(editedElem).attr(propName,propValue);
+    }
+}
+
 $(".demo--live").each ( function(){
     var svgCode = $(this).find(".demo__code");
     var shape_code = svgCode.val();
@@ -50,7 +70,7 @@ $(".demo--live").each ( function(){
 
 });
 
-// Show Demos
+// Switch Demos
 // -------------------------------
 var switchCheckedClass = "switch--checked";
 $(".demo__switch dd").each( function(){
@@ -58,18 +78,27 @@ $(".demo__switch dd").each( function(){
         $("." + switchCheckedClass, $(this).parent()).removeClass(switchCheckedClass);
         $(this).addClass( switchCheckedClass );
 
+        console.log("**");
         var changeAttr = $(this).parent().attr("data-attr");
+        var changeDest = $(this).parent().attr("data-dest");
+        if( !changeDest ){
+            changeDest = "g";
+            }
         var changeVal = $(this).attr("data-val");
 
-        var svgGroup = $(curSlideClass).find("g");
-        svgGroup.attr(changeAttr,changeVal);
+        var svgDest = $(curSlideClass).find(changeDest);
 
-        var svgElem = $(curSlideClass).find("svg");
-        var svgElem_n = document.querySelector(".active svg");
-        var svgViewbox = svgElem_n.getAttribute("viewBox"); //svg.attr("viewBox");
-        var svgContent = svgElem.html();
-        var demoCode = $(curSlideClass).find(".demo__code");
-        demoCode.val("<svg viewbox='" + svgViewbox + "'>" + svgContent + "</svg>");
+        console.log("svgDest");
+        console.log(svgDest);
+        //svgDest.attr(changeAttr,changeVal);
+        setAttr(changeAttr, changeVal, svgDest);
+
+        // var svgElem = $(curSlideClass).find("svg");
+        // var svgElem_n = document.querySelector(".active svg");
+        // var svgViewbox = svgElem_n.getAttribute("viewBox"); //svg.attr("viewBox");
+        // var svgContent = svgElem.html();
+        // var demoCode = $(curSlideClass).find(".demo__code");
+        // demoCode.val("<svg viewbox='" + svgViewbox + "'>" + svgContent + "</svg>");
         });
     });
 
@@ -116,19 +145,41 @@ $(".js-toggle-code").click(function(){
 // Edit Property
 // -------------------------------
 
-function editProperty(elem) {
-    console.log(elem);
-    var editedCode = $(elem).find(".demo__code");
-    var svgElem = $(elem).find("svg");
-    var editedElem = $(elem).find(".demo__live-edit");
+function editProperty(elem, editedCode) {
 
-    var propsValues = editedCode.val().split(" ");
+    var svgElem = $(elem).find("svg");
+    var codeVal = $(editedCode).val();
+    var maxLine = 64;
+
+    if (!codeVal) return;
+
+    if ($(editedCode).val().length > maxLine){
+        console.log("too long");
+        $(editedCode).addClass("code--two-lines");
+    }
+
+    var destination = ".demo__live-edit";
+    if ( $(editedCode).attr("data-dest") ){
+        destination = "." + $(editedCode).attr("data-dest");
+        }
+    var editedElem = $(elem).find(destination);
+
+    var propsValues = $(editedCode).val().split("\" ");
+
+    //console.log("propsValues");
+    //console.log(propsValues);
 
     for (var i = 0; i < propsValues.length; i++) {
         var propNameValue = propsValues[i].split("=");
-        var propName = propNameValue[0];
-        var propValue = propNameValue[1].replace(/"/g,"");
-        $(editedElem).attr(propName,propValue);
+        if (propNameValue[0]) {
+
+            var propName = propNameValue[0];
+            var propValue = propNameValue[1].replace(/"/g,"");
+
+            var svgElem = document.querySelector(".active svg");
+
+            setAttr(propName, propValue, editedElem);
+        }
     }
 }
 
@@ -137,12 +188,14 @@ $(".demo--editproperty").each ( function(){
     var codeParent = this;
 
     editedCode.change ( function(codeParent){
-        editProperty(codeParent);
+        editProperty(codeParent, this);
+        // console.log("change");
     });
 
-    $(editedCode).bind('keydown keyup', function(event){
+    $(editedCode).bind("keydown keyup", function(event){
         event.stopPropagation();
-        editProperty(codeParent);
+        editProperty(codeParent, this);
+        // console.log("keydown keyup");
     });
 
 });
