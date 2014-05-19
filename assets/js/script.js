42a242fe772aee435dd080429c601bc482288f37
yoksel <yoksel@yandex.ru>
2014-05-19 22:50:40 +0400

    Save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,11 +83,11 @@ $(".demo--live").each ( function(){
 // Resize Objects
 // -------------------------------
 
-$(".demo__slider").change(function(){
-  console.log("hello");
-  console.log($(this).val());
-  $(".resize-cat").css("width",$(this).val() + "%");
-});
+// $(".demo__slider").change(function(){
+//   console.log("hello");
+//   console.log($(this).val());
+//   $(".resize-cat").css("width",$(this).val() + "%");
+// });
 
 // Switch Demos
 // -------------------------------
