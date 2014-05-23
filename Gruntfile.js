09a32c360d2ca91437665872fb2dab4a49090a0e
yoksel <yoksel@yandex.ru>
2014-05-23 21:42:42 +0400

    Save changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,7 +141,7 @@ module.exports = function (grunt) {
     watch: {
       css: {
         files: '<%= project.css_src %>{,*/}*.{scss,sass}',
-        tasks: ['sass:dev', 'autoprefixer','includereplace:dev'],
+        tasks: ['includereplace:dev','sass:dev', 'autoprefixer'],
         options: {
           livereload: 35731,
         },
